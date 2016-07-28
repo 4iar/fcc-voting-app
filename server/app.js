3a64d0a03654a396e7a71b89b51128c224a63d1c
4iar <4iar@users.noreply.github.com>
2016-07-29 00:12:43 +0100

    Add route scaffolding

--- server/app.js
+++ server/app.js
@@ -13,6 +13,25 @@ app.get('/', (request, response) => {
   response.json({hello: "world"});
 })
 
+app.post('/api/user/create', (request, response) => {
+  // register a new user
+})
+
+app.post('/api/poll/create', (request, response) => {
+  // get json new poll stuff
+})
+
+app.get('/api/poll/:pollId/view', (request, response) => {
+})
+
+app.post('/api/poll/:id/delete', (request, response) => {
+})
+
+app.post('/api/poll/:id/vote', (request, response) => {
+  // add vote to the option
+  // or create new option if it does not exist
+})
+
 MongoClient.connect(mongolabUri, (err, database) => {
   if (err) return console.log(err)
   db = database
