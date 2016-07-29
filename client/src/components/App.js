0a83629296aa70643ff6c0ef8c367d9dbc1dcf92
4iar <4iar@users.noreply.github.com>
2016-07-29 17:44:17 +0100

    Add basic navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import {Navbar, NavItem, Nav} from 'react-bootstrap';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>alskjdlfkjalksjd</h1>
+        <Navbar inverse>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Voting App</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="#">Not logged in</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
         {this.props.children}
       </div>
     );
