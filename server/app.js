812e8d8db2b6bbf8f18c4dd8b4e1e23ece5d094f
4iar <4iar@users.noreply.github.com>
2016-07-29 19:21:58 +0100

    for testing

--- server/app.js
+++ server/app.js
@@ -12,6 +12,12 @@ const app = express();
 
 app.use(bodyParser.json({ extended: true }))
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 const port = process.env.PORT || 5000; // let Heroku set the port
 
