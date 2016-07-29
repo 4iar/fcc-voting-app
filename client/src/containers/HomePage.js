58ac83d8f5f2c1fd206e5a7441ca02608f505da0
4iar <4iar@users.noreply.github.com>
2016-07-29 18:06:41 +0100

    Implement viewing of polls in the homepage (no redux logic yet)

--- client/src/containers/HomePage.js
+++ client/src/containers/HomePage.js
@@ -1,19 +1,37 @@
 import React from 'react';
+import {Col, Grid, Row, ListGroup, ListGroupItem} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
 
 export default class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {polls: [{id: 40302, description: 'question description', question: 'question title'}]};
+  }
+
   render() {
     return (
-      <div>
-        alskdjlkjlasd
-        a
-        sd
-        f
-        a
-        sd
-        f
-
-        <h2>homepage</h2>
-      </div>
+      <Grid>
+        <Row>
+          <Col mdOffset={2} md={8}>
+            <div>
+              <ListGroup>
+                {this.state.polls.map((poll) => {
+                  const url = '/poll/' + poll.id;
+                  return (
+                    <LinkContainer key={poll.id} to={{pathname: url}}>
+                      <ListGroupItem header={poll.question}>
+                        {poll.description}
+                      </ListGroupItem>
+                    </LinkContainer>
+                  );
+                })}
+              </ListGroup>
+            </div>
+          </Col>
+          <Col md={2}/>
+        </Row>
+      </Grid>
     );
   }
 }
