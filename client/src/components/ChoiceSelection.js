16a01c6dd18582843d237a69e3030fd957bf404a
4iar <4iar@users.noreply.github.com>
2016-07-30 17:03:33 +0100

    Refactor: move functions out of render method

--- client/src/components/ChoiceSelection.js
+++ client/src/components/ChoiceSelection.js
@@ -11,47 +11,45 @@ export default class ChoiceSelection extends React.Component {
   constructor(props) {
     super(props);
 
-    // TODO: usen endpoint constant -- is annoying because no easy string formatting...
+    // TODO: use endpoint constant -- is annoying because no easy string formatting...
     this.submitVoteEndpoint = BASE_URL + '/api/poll/' + this.props.pollId + '/vote';
 
     this.choice = '';
+  }
+  
+  logChange(choice) {
+    this.choice = choice;
+  }
+  
+  handleSubmit(e) {
+    e.preventDefault();  // stop redirection on form submit
+    $.ajax({
+      type: 'POST',
+      url: this.submitVoteEndpoint,
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({'choice': this.choice})
+    })
+      .done(function(data) {
+      })
+      .fail(function(jqXhr) {
+      });
   }
 
   render() {
     const options = this.props.choices.map((choice) => {
       return {value: choice, label: choice}
     });
-
-    function logChange(choice) {
-      this.choice = choice;
-    }
-
-    function handleSubmit(e) {
-      e.preventDefault();  // stop redirection on form submit
-      console.log(this.choice);
-      console.log(e);
-      $.ajax({
-        type: 'POST',
-        url: this.submitVoteEndpoint,
-        contentType: 'application/json',
-        dataType: 'json',
-        data: JSON.stringify({'choice': this.choice})
-      })
-        .done(function(data) {
-        })
-        .fail(function(jqXhr) {
-        });
-    }
-
+    
     return (
       <div>
-        <form onSubmit={handleSubmit.bind(this)}>
+        <form onSubmit={this.handleSubmit.bind(this)}>
           <FormGroup>
             <Select
               name="choice"
               value="one"
               options={options}
-              onChange={logChange.bind(this)}
+              onChange={this.logChange.bind(this)}
               allowCreate={true}
             />
           </FormGroup>
