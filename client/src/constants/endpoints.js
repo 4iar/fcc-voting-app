d078ae8eb4cc046db266193a0508aba509e8d174
4iar <4iar@users.noreply.github.com>
2016-07-31 17:08:14 +0100

    Add placeholder endpoints

--- client/src/constants/endpoints.js
+++ client/src/constants/endpoints.js
@@ -1,2 +1,4 @@
-const base = 'https://voting-app-4iar.herokuapp.com';
-export const ALL_POLLS_ENDPOINT = base + '/api/polls/view';
+//export const BASE_URL = 'https://voting-app-4iar.herokuapp.com';
+export const BASE_URL = 'http://localhost:5000';
+export const ALL_POLLS_ENDPOINT = BASE_URL + '/api/polls/view';
+export const VOTE_ENDPOINT = BASE_URL + '/api/poll/pollId/vote'; //fucking no string formatting
