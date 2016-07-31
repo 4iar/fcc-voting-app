b6362c4001b1ca6a270b3df13061615ddd1cd50f
4iar <4iar@users.noreply.github.com>
2016-07-31 21:49:28 +0100

    Add rendering logic for user polls pages

--- client/src/containers/UserPollsPage.js
+++ client/src/containers/UserPollsPage.js
@@ -1,9 +1,71 @@
 import React from 'react';
+import {Col, Grid, Row, ListGroup, ListGroupItem, PageHeader} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
+import $ from 'jquery';
+
+import {BASE_URL} from '../constants/endpoints';
 
 export default class UserPollsPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.user = this.props.params.user;
+    this.state = {
+      polls: []
+    }
+
+    this.userPollsEndpoint = BASE_URL + '/api/user/' + this.user + '/polls';
+  }
+
+  componentDidMount() {
+    this.serverRequest = $.get(this.userPollsEndpoint, function (result) {
+      this.setState({
+        polls: result
+      }, console.log(this.state));
+    }.bind(this));
+    
+  }
+
+  componentWillUnmount() {
+    this.serverRequest.abort();
+  }
+
   render() {
+    // TODO: factor this out with HomePage into a dumb component
+    let pollsIndex;
+    if (!this.state.polls) {
+      pollsIndex = (
+        <h2>Loading</h2>
+      )
+    } else {
+      pollsIndex = Object.keys(this.state.polls).map((pollId) => {
+        const poll = this.state.polls[pollId];
+        const url = '/poll/' + poll.id;
+        return (
+          <LinkContainer key={poll.id} to={{pathname: url}}>
+            <ListGroupItem header={poll.question}>
+              {poll.description}
+            </ListGroupItem>
+          </LinkContainer>
+        );
+      })
+    }
+
     return (
-      <h1>User polls page</h1>
+      <Grid>
+        <br/>
+        <Row>
+          <Col mdOffset={2} md={8}>
+            <PageHeader>Polls created by {this.user}</PageHeader>
+            <div>
+              <ListGroup>
+                {pollsIndex}
+              </ListGroup>
+            </div>
+          </Col>
+          <Col md={2}/>
+        </Row>
+      </Grid>
     );
   }
 }
