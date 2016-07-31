8b12c9829cbd93fa4f23386f01dad8e45cfee089
4iar <4iar@users.noreply.github.com>
2016-07-31 23:26:10 +0100

    Connect the poll deletion button to api and handle outcomes

--- client/src/components/DeletePoll.js
+++ client/src/components/DeletePoll.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import {Button, Col, Glyphicon, Row} from 'react-bootstrap';
+import {Button, Col, Glyphicon, Row, Panel} from 'react-bootstrap';
+import $ from 'jquery';
+
+import {BASE_URL} from '../constants/endpoints';
 
 
 export default class DeletePoll extends React.Component {
   constructor(props) {
     super(props);
 
+    this.pollId = this.props.pollId;
+    this.deleteEndpoint = BASE_URL + '/api/poll/' + this.pollId + '/delete';
     this.state = {
-      warning: 0
+      warning: 0,
+      error: '',
+      deleted: false
     };
   }
 
   handleClick() {
+    // make the user click the delete button a second time to confirm
     if (this.state.warning === 1) {
-      //delete calls here
+      $.ajax({
+        type: 'GET',
+        url: this.deleteEndpoint,
+        success: this.handleDeleteSuccess.bind(this),  // one day
+        error: this.handleDeleteError.bind(this)
+      });
     } else {
       this.setState({
         warning: this.state.warning + 1
@@ -21,13 +34,43 @@ export default class DeletePoll extends React.Component {
     }
   }
 
+  handleDeleteSuccess(data) {
+    if (data.status === "success") {
+      this.setState({
+        deleted: true
+      });
+    } else {
+      this.setState({
+        error: "Error deleting poll"
+      });
+    }
+  }
+
+  handleDeleteError(data) {
+    this.setState({
+      error: "Failed to contact the server"
+    })
+  }
+
   render() {
+    const pollDeleted = <h1>Poll has been deleted</h1>
+    const error = <Panel header="Error :(" bsStyle="danger">{this.state.error}</Panel>
+
     return (
-      <Row>
-        <Col mdOffset={11} md={1}>
-          <Button onClick={this.handleClick.bind(this)} bsStyle={this.state.warning ? 'danger' : ''}><Glyphicon glyph="trash"/></Button>
-        </Col>
-      </Row>
+      <div>
+        <Row>
+          <Col mdOffset={2} md={8}>
+            {this.state.deleted ? pollDeleted : null}
+            {this.state.error ? error : null}
+          </Col>
+          <Col md={2}></Col>
+        </Row>
+        <Row>
+          <Col mdOffset={11} md={1}>
+            <Button onClick={this.handleClick.bind(this)} bsStyle={this.state.warning ? 'danger' : ''}><Glyphicon glyph="trash"/></Button>
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
