ab300eb443860e66e6bd49b28cbf1c0cc8b86f88
4iar <4iar@users.noreply.github.com>
2016-08-02 00:56:35 +0100

    Add frontend login handling

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,13 @@
+/*global Auth0Lock */
 import React from 'react';
 import {Navbar, NavItem, Nav, Glyphicon} from 'react-bootstrap';
 import {connect} from 'react-redux';
 import {LinkContainer} from 'react-router-bootstrap';
 import $ from 'jquery';
 
-import {ALL_POLLS_ENDPOINT} from '../constants/endpoints';
+import {ALL_POLLS_ENDPOINT, BASE_URL} from '../constants/endpoints';
 import {setPolls} from '../actions/appActions';
+
 
 function getState(state) {
   return {
@@ -18,10 +20,10 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
 
+
     this.state = {
       user: this.props.user
     };
-
   }
 
   componentDidMount() {
@@ -39,6 +41,20 @@ export default class App extends React.Component {
     this.setState({
       user: newProps.user
     });
+  }
+
+  showLogin() {
+    const lock = new Auth0Lock('Y0HjKDY8epVefNvJlwcUfvsykX9jckfb', 'fcc-voting-app-4iar.auth0.com', {
+      auth: {
+        redirectUrl: BASE_URL + '/callback',
+        responseType: 'code',
+        params: {
+          scope: 'openid email' // Learn about scopes: https://auth0.com/docs/scopes
+        }
+      }
+    });
+    
+    lock.show();
   }
 
   render() {
@@ -63,6 +79,7 @@ export default class App extends React.Component {
               <LinkContainer to={{ pathname: '/user/' + this.state.user }}>
                 <NavItem eventKey={2} href="#">{this.state.user || "Not logged in"}</NavItem>
               </LinkContainer>
+              <NavItem onClick={this.showLogin.bind(this)} eventKey={3}>Login button placeholder</NavItem>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
